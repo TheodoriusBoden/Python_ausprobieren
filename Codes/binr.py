80795fc8baa150f12297ed60fb6cda3e3d42800a
TheodoriusBoden <theodor.bode@icloud.com>
2025-09-21 22:31:10 +0200

    Update binr.py

--- Codes/binr.py
+++ Codes/binr.py
@@ -1,4 +1,4 @@
-dezi = int(input("Welche dezimalzahl möchtest du in Binärcode umwandeln?"))
+dezi = int(input("Welche Dezimalzahl möchtest du in Binärcode umwandeln?"))
 binr = bin(dezi)
 
 print(binr)
