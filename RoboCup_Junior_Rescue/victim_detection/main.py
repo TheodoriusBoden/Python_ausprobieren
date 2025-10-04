b43ff07da5665e0ce8cda313b9e7817ed687e682
TheodoriusBoden <theodor.bode@icloud.com>
2025-10-04 21:48:52 +0200

    lutxs

--- RoboCup_Junior_Rescue/victim_detection/main.py
+++ RoboCup_Junior_Rescue/victim_detection/main.py
@@ -4,4 +4,4 @@ from ultralytics import YOLO
 model = YOLO("yolov8n.yaml") # built a new model from scratch
 
 # Use the model
-results = model.train(data="Codes/RoboCup_Junior_Rescue/victim_detection/config.yaml", epochs = 1) # train the model
+results = model.train(data="/home/ag/Dokumente/GitHub/Python_ausprobieren/RoboCup_Junior_Rescue/victim_detection/config.yaml", epochs = 1) # train the model
