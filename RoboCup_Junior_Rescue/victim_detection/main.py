b25e285d8948912aa529738ea07191078ced564b
TheodoriusBoden <theodor.bode@icloud.com>
2025-10-04 20:35:26 +0200

    started training

--- RoboCup_Junior_Rescue/victim_detection/main.py
+++ RoboCup_Junior_Rescue/victim_detection/main.py
@@ -4,4 +4,4 @@ from ultralytics import YOLO
 model = YOLO("yolov8n.yaml") # built a new model from scratch
 
 # Use the model
-results = model.train(data="coco128.yaml", epochs = 3) # train the model
+results = model.train(data="Codes/RoboCup_Junior_Rescue/victim_detection/config.yaml", epochs = 1) # train the model
